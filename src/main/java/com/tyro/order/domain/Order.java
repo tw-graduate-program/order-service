cc7858655214aeeba666e87d63a6c13fe6d2fa17
LYabstract <710116572@qq.com>
2023-03-18 17:08:37 +0800

    [007] feat: create order domain

---
+++ src/main/java/com/tyro/order/domain/Order.java
@@ -0,0 +1,51 @@
+package com.tyro.order.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@Entity(name = "order_info")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotNull
+    private String contactId;
+
+    private Integer orderType;
+
+    private Integer orderStatus;
+
+    private BigDecimal orderFee;
+
+    private BigDecimal deliveryFee;
+
+    @CreatedDate
+    @JsonIgnore
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @JsonIgnore
+    private Instant updatedAt;
+
+    @JsonIgnore
+    private boolean deleted = false;
+}
