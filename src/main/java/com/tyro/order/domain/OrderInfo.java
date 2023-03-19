34d51c9532c29ec270072758772dfd82797f45ee
LYabstract <710116572@qq.com>
2023-03-19 12:11:23 +0800

    [007] fix: modify order entity and database table

--- src/main/java/com/tyro/order/domain/OrderInfo.java
+++ src/main/java/com/tyro/order/domain/OrderInfo.java
@@ -8,24 +8,21 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.Instant;
 
 @Data
-@Entity(name = "order_info")
+@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+public class OrderInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @NotNull
     private String contactId;
--- src/main/resources/db/migration/V2023_03_18__create_order_info_table.sql
+++ src/main/resources/db/migration/V2023_03_18__create_order_info_table.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS `order_info`;
 
-CREATE TABLE `order_info` (
-    `id`            VARCHAR(36)  PRIMARY KEY NOT NULL,
+CREATE TABLE `order_info`
+(
+    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT,
     `contact_id`    VARCHAR(36)  NOT NULL,
     `order_type`    INT(11),
     `order_status`  INT(4),
@@ -9,8 +10,7 @@ CREATE TABLE `order_info` (
     `delivery_fee`  DECIMAL(18,2),
     `created_at`    DATETIME     NULL,
     `updated_at`    DATETIME     NULL,
-    `deleted`       BOOLEAN      DEFAULT FALSE NOT NULL
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4;
+    `deleted`       BOOLEAN      DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
