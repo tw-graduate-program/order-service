43e3d23c8d61b1682ab191ee47587ae8eb31163f
LYabstract <710116572@qq.com>
2023-06-10 17:10:39 +0800

    [007] fix: filter result by use lambda stream

--- src/main/java/com/demo/order/service/OrderService.java
+++ src/main/java/com/demo/order/service/OrderService.java
@@ -5,6 +5,7 @@ import com.demo.order.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -16,7 +17,10 @@ public class OrderService {
     }
 
     public List<OrderInfo> getAllOrders() {
-        return orderRepository.getAllOrders();
+        List<OrderInfo> orderInfos = orderRepository.getAllOrders().stream()
+                .filter(orderInfo -> orderInfo.getOrderStatus().compareTo(Integer.valueOf(1)) == 0)
+                .collect(Collectors.toList());
+        return orderInfos;
     }
 
     public OrderInfo getOrderById(Long id) {
